8f57c8b059d5796fd70f830faabc291d31facf15
Sergey <rogozhinis@mail.ru>
2022-12-19 20:12:01 +0500

    Задача 1 Квадрат ли?

---
+++ Семинар1/Console_up2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу,которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+
+// Запрос числа от пользователя.
+
+Console.WriteLine("Введите первое число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Число a является квадратом числа b");
+}
+else
+{
+    Console.WriteLine("Число a не является квадратом числа b");
+ }
